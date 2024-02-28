cbf2d60eef0abc9222fccffc611fd93baea2428e
ShiriNmi1520 <s20026352@gmail.com>
2024-02-28 11:05:20 +0800

    :lipstick: Quote 更改為單引號

--- packages/app/features/creator/creator.tsx
+++ packages/app/features/creator/creator.tsx
@@ -14,45 +14,43 @@ import { useState } from 'react'
 
 export function CreatorSection() {
   return (
-    <YStack f={1} jc="center" ai="center" p="$4">
-      <H1 ta="left">Meet your creator</H1>
-      <XStack alignItems="center" space="$6">
+    <YStack f={1} jc='center' ai='center' p='$4'>
+      <H1 ta='left'>Meet your creator</H1>
+      <XStack alignItems='center' space='$6'>
         <Dialog modal>
           <Dialog.Trigger asChild>
-            <Avatar circular size="$10">
+            <Avatar circular size='$10'>
               <Avatar.Image
-                accessibilityLabel="Test1"
-                src="https://media.discordapp.net/attachments/737897046953951235/1212004369323794482/IMG_8669.jpg?ex=65f04207&is=65ddcd07&hm=c72e211196222ce293bf4b74f5b84114c4c39d5717580207a3af42afac732035&=&format=webp&width=865&height=649"
+                accessibilityLabel='Test1'
+                source={{ uri: 'https://media.discordapp.net/attachments/737897046953951235/1212004369323794482/IMG_8669.jpg?ex=65f04207&is=65ddcd07&hm=c72e211196222ce293bf4b74f5b84114c4c39d5717580207a3af42afac732035&=&format=webp&width=865&height=649' }}
               />
-              <Avatar.Fallback backgroundColor="$blue10" />
+              <Avatar.Fallback backgroundColor='$blue10' />
             </Avatar>
           </Dialog.Trigger>
-          <Adapt when="sm" platform="touch">
-            <Sheet animation="medium" zIndex={200000} modal dismissOnSnapToBottom>
-              <Sheet.Frame padding="$4" gap="$4">
+          <Adapt when='sm' platform='touch'>
+            <Sheet animation='medium' zIndex={200000} modal dismissOnSnapToBottom>
+              <Sheet.Frame padding='$4' gap='$4'>
                 <Adapt.Contents />
               </Sheet.Frame>
               <Sheet.Overlay
-                animation="lazy"
+                animation='lazy'
                 enterStyle={{ opacity: 0 }}
                 exitStyle={{ opacity: 0 }}
               />
             </Sheet>
           </Adapt>
-
           <Dialog.Portal>
             <Dialog.Overlay
-              key="overlay"
-              animation="slow"
+              key='overlay'
+              animation='slow'
               opacity={0.5}
               enterStyle={{ opacity: 0 }}
               exitStyle={{ opacity: 0 }}
             />
-
             <Dialog.Content
               bordered
               elevate
-              key="content"
+              key='content'
               animateOnly={['transform', 'opacity']}
               animation={[
                 'quick',
@@ -64,7 +62,7 @@ export function CreatorSection() {
               ]}
               enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
               exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
-              gap="$4"
+              gap='$4'
             >
               <Dialog.Title>Thootau</Dialog.Title>
               <Dialog.Description>
@@ -73,10 +71,10 @@ export function CreatorSection() {
               <Unspaced>
                 <Dialog.Close asChild>
                   <Button
-                    position="absolute"
-                    top="$3"
-                    right="$3"
-                    size="$2"
+                    position='absolute'
+                    top='$3'
+                    right='$3'
+                    size='$2'
                     circular
                     icon={X}
                   />
@@ -85,13 +83,6 @@ export function CreatorSection() {
             </Dialog.Content>
           </Dialog.Portal>
         </Dialog>
-        <Avatar circular size="$10">
-          <Avatar.Image
-            accessibilityLabel="Test1"
-            src="https://avatars.githubusercontent.com/u/1"
-          />
-          <Avatar.Fallback backgroundColor="$blue10" />
-        </Avatar>
       </XStack>
     </YStack>
   )
